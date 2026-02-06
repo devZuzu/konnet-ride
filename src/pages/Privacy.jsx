4c200b4f61dab912f65711cb71a34ef8608eb89c
devZuzu <zulaihamuhammadtiamiyu@gmail.com>
2026-02-06 12:53:24 +0100

    Update hero styles, privacy & terms pages

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import "./legal.css";
+import "./Legal.css";
 
 const Privacy = () => {
   return (
--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import "./Legal.css";
+
 
 const Terms = () => {
   return (
